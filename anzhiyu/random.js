abf9c6fdf8879326011a66dcc2b4384a732a8658
叶蓝骅 <lanhua.ye@icloud.com>
2025-02-28 09:53:45 +0300

    Site updated: 2025-02-28 09:53:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/24/Theoretical-knowledge/","2025/02/28/lab1/","2025/01/25/limit/"];function toRandomPost(){
+var posts=["2025/02/28/lab1/","2025/01/25/limit/","2025/02/24/Theoretical-knowledge/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
